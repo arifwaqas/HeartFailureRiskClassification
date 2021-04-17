8c0337be52b21369416937bb64dfb9c177833b5b
arifwaqas <awakenarif@gmail.com>
2021-04-17 06:49:32 +0545

    Corrected HTTP response glitch

--- server/index.js
+++ server/index.js
@@ -12,12 +12,12 @@ app.use(express.urlencoded({extended: true})); //req.body < data
 app.post('/compute', (req, res) => { //route
     const { age, cre, dia, eje, bp, plt, ser, seso ,secre, sex, smok, time} = req.body;
     
-    let result = R.callMethod("C:/Users/waqas/Desktop/model_created_rahul_rippen_modification.R", "predictor2", [age, ser,cre, dia,eje,bp,plt,seso,secre,sex,smok,time]);
+    let result = R.callMethod("C:/Users/waqas/Desktop/model_created_rahul_rippen_modification.R", "predictor2", [age, cre, dia, eje, bp, plt, ser, seso ,secre, sex, smok, time]);
     console.log(result[1]);
-    if (result[1] == 0){
+    if (result[1] == 1){
         res.send("HIGH chance of fatality");
     }
-    res.send("LOW chance of fatality");
+    else res.send("LOW chance of fatality");
 });
 
 app.listen(process.env.PORT, () => console.log(`Server started on port: ${process.env.PORT}`));
